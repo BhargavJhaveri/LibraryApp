5fa93774c077ba090862d5439811091dc8f7ec2b
viswa <viswaraavi@yahoo.in>
2015-09-25 19:13:33 -0400

    htriu

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -102,6 +102,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:ISBN,:TITLE,:AUTHORS,:DESCRIPTION)
+    params.require(:book).permit(:ISBN,:TITLE,:AUTHORS,:DESCRIPTION,:STATUS)
   end
 end
